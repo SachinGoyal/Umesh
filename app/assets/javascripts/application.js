ef442c15820549ff04e9328ea3cde17418c4da7a
sachin <goyalsachin52@gmail.com>
2014-11-20 12:22:17 +0530

    gitignore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+  $('form').on 'click', '.remove_fields', (event) ->
+    $(this).closest('.field').remove()
+    event.preventDefault()
 
+  $('form').on 'click', '.add_fields', (event) ->
+    time = new Date().getTime()
+    regexp = new RegExp($(this).data('id'), 'g')
+    $(this).before($(this).data('fields').replace(regexp, time))
+    event.preventDefault()
