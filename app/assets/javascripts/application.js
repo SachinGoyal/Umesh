d82923a46df9f6520540475431d739f38b02f380
sachin <goyalsachin52@gmail.com>
2014-11-20 15:53:18 +0530

    cofee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,17 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
-  $('form').on 'click', '.remove_fields', (event) ->
-    $(this).closest('.field').remove()
-    event.preventDefault()
-
-  $('form').on 'click', '.add_fields', (event) ->
-    time = new Date().getTime()
-    regexp = new RegExp($(this).data('id'), 'g')
-    $(this).before($(this).data('fields').replace(regexp, time))
-    event.preventDefault()
+ 
+jQuery(function() {
+  $('form').on('click', '.remove_fields', function(event) {
+    $(this).closest('.field').remove();
+    return event.preventDefault();
+  });
+  return $('form').on('click', '.add_fields', function(event) {
+    var regexp, time;
+    time = new Date().getTime();
+    regexp = new RegExp($(this).data('id'), 'g');
+    $(this).before($(this).data('fields').replace(regexp, time));
+    return event.preventDefault();
+  });
+});
